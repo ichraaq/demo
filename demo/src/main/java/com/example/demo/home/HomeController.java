d798e40062069cf2f8d4cb6eda770cd7d9de75a6
dell <dell@DESKTOP-2G354G4>
2020-12-17 16:11:55 +0100

    first commit

---
+++ demo/src/main/java/com/example/demo/home/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String homePage(Model model)
+	{
+		return "frontend";
+	}
+
+}
